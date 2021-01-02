19ff9f032584ab054a239f7b635126433ad9f241
AmiraRashad <72689422+AmiraRashad@users.noreply.github.com>
2021-01-02 21:16:53 +0500

    Update Generic.js

--- src/services/Generic.js
+++ src/services/Generic.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //axios.defaults.baseURL="http://localhost:4000/api/";
-axios.defaults.baseURL="https://layers-app-api.herokuapp.com/";
+axios.defaults.baseURL="https://layers-app-api.herokuapp.com/api";
 
 axios.defaults.headers.common["x-auth-token"]=localStorage.getItem("token");
 class Generic{
